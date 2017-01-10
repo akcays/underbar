b470f6dddc00d0ba035163e86efeb61b1b05642c
ilke <ilkesedefakcay@gmail.com>
2017-01-09 22:08:56 -0800

    Optimized 'reject' function

--- src/underbar.js
+++ src/underbar.js
@@ -80,11 +80,7 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    var result = [];
-    _.each(collection, function(item, index) {
-      if (!test(item)) result.push(item);
-    });
-    return result;
+    return _.filter(collection, item => !test(item));
   };
 
   // Produce a duplicate-free version of the array.
